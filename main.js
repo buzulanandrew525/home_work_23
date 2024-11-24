b08c56ce0306d71c9d8b857fffc0519180f04350
buzulanandrew525 <buzulanandrew525@gmail.com>
2024-11-24 15:36:51 +0200

    fixed errors

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ console.log('#6. JavaScript homework example file')
  * Внутрішнє сховище продуктів має бути реалізоване за допомогою new Map().
  * Наявність методів addProduct, getProductCalories, та removeProduct.
  */
-
+console.log(`Завдання №1`);
+document.write(`Завдання №1 <br>`);
 class CalorieCalculator {
   constructor() {
     this.products = new Map();
@@ -50,10 +51,13 @@ calorieCalculator.addProduct('Apple', 52);
 calorieCalculator.addProduct('Banana', 89);
 
 console.log(calorieCalculator.getProductCalories('Apple')); // 52
+document.write(calorieCalculator.getProductCalories('Apple') + '<br>');
 console.log(calorieCalculator.getProductCalories('Banana')); // 89
+document.write(calorieCalculator.getProductCalories('Banana') + '<br>');
 
 calorieCalculator.removeProduct('Apple');
 console.log(calorieCalculator.getProductCalories('Apple')); // Product not found
+document.write(calorieCalculator.getProductCalories('Apple') + '<br>' + '<br>');
 
 /*
  * #2
@@ -68,7 +72,8 @@ console.log(calorieCalculator.getProductCalories('Apple')); // Product not found
  * Використання конструкції class для створення класу UniqueUsernames.
  * Застосування new Set() для внутрішнього сховища імен користувачів у конструкторі класу.
  */
-
+console.log(`Завдання №2`);
+document.write(`Завдання №2 <br>`);
 class UniqueUsernames {
   constructor() {
     this.usernames = new Set();
@@ -77,15 +82,18 @@ class UniqueUsernames {
   addUser(username) {
     if (this.usernames.has(username)) {
       console.log(`Користувач "${username}" вже існує.`);
+      document.write(`Користувач "${username}" вже існує. <br>`);
     } else {
       this.usernames.add(username);
       console.log(`Користувач "${username}" успішно доданий.`);
+      document.write(`Користувач "${username}" успішно доданий. <br>`);
     };
   };
 
   exists(username) {
     if (this.usernames.has(username)) {
       console.log(`Користувач "${username}" не знайден.`);
+      document.write(`Користувач "${username}" не знайден. <br>`);
     };
   };
 
@@ -101,6 +109,8 @@ uniqueUsernames.addUser('jane_doe');
 uniqueUsernames.addUser('john_doe'); // Ця дія не змінить набір, оскільки 'john_doe' вже існує
 
 console.log(`Існує 'john_doe': ${uniqueUsernames.exists('john_doe')}`); // true
+document.write(`Існує 'john_doe': ${uniqueUsernames.exists('john_doe')} <br>`);
 console.log(`Кількість унікальних імен: ${uniqueUsernames.count()}`); // 2
+document.write(`Кількість унікальних імен: ${uniqueUsernames.count()} <br>`);
 
 // Експорт для використання в тестах
